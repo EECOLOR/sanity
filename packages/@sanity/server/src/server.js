6678d830d19080c907e0599cf2327d9c0015eeae
EECOLOR <eecolor@qirx.org>
2019-05-08 21:14:20 +0200

    create a smaller API footprint by removing exports

--- packages/@sanity/server/src/server.js
+++ packages/@sanity/server/src/server.js
@@ -1,10 +1,8 @@
 import ReactDOM from 'react-dom/server'
 import webpack from 'webpack'
 import getDevServer from './devServer'
-import getProdServer from './prodServer'
 import getWebpackProdConfig from './configs/webpack.config.prod'
 import getWebpackDevConfig from './configs/webpack.config.dev'
-import getWebpackBaseConfig from './configs/webpack.config'
 import applyStaticLoaderFix from './util/applyStaticLoaderFix'
 import {getDocumentElement} from './baseServer'
 
@@ -20,11 +18,8 @@ const getWebpackCompiler = baseConfig => {
 // exported to allow reuse of same React version from CLI
 export {
   getDevServer,
-  getProdServer,
   getWebpackCompiler,
   getWebpackDevConfig,
-  getWebpackBaseConfig,
-  getWebpackProdConfig,
   getDocumentElement,
   applyStaticLoaderFix,
   ReactDOM
